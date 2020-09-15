13be2a54b751842dc1237ca2fc87b29b223bacd8
Kigaroth <hp_20@hotmail.com>
2020-09-15 14:56:59 +0200

    Changed with Vectors instead of array

--- Class - Session 3/Class - Session 3.cpp
+++ Class - Session 3/Class - Session 3.cpp
@@ -7,6 +7,69 @@
 
 int main()
 {
+    //new code using vectors//
     
+    const int size = 10;
+    int my_array[size] = {};
+
+    std::vector <char> board = { '1','2','3','4','5','6','7','8','9' };
+ 
+        for (int i = 0; i < sizeof(board) / sizeof(board[0]); i++) { //i should use the size of the board instead//
+            if (i % 3 == 0) {
+                std::cout << "\n";
+            }
+            std::cout << board[i];
+            std::cout << "\t ";
+        }
+
+        return 0;
 }
+
+        //old ccode//
+        //const int size = 10;
+        //int my_array[size] = {};
+
+        //char board[9] = { '1','2','3','4','5','6','7','8','9' };
+        //std::cout << sizeof(board) / sizeof(board[0]);
+
+        //int board2[22] = {};
+        //std::cout << sizeof(board2) / sizeof(board2[0]);
+        //std::cout << "Count of :" << _countof(board2);
+
+        //for (int i = 0; i < sizeof(board) / sizeof(board[0]); i++) { //i should use the size of the board instead//
+        //    if (i % 3 == 0) {
+        //        std::cout << "\n\n";
+        //    }
+        //    std::cout << board[i];
+        //    std::cout << "\t ";
+        //}
+
+
+
+//char board[9]; {'1','2','3','4','5','6','7','8','9'}  --> Written on blackboard in class
+//double num [100] = {} --> Written on blackboard in class
+
+//This is bad practice for an array, better solution is above//
+
+    //for (int i = 0; i < 9; i++) {
+    //    if (i % 3 == 0) {
+    //        std::cout << "\n\n";
+    //    }
+    //    std::cout << board[i];
+    //    std::cout << "\t ";
+    //}
+
+//
+//#include <iostream>
+//#include <vector>
+//
+//int main()
+//{
+//    /*std::vector <int> numbers;
+//    std::vector <char> ;*/
+//
+//
+//    std::vector <char> board = { '1','2','3','4' };
+//    std::cout << board.at(2) << std::endl;
+
 
